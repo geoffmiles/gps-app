b4d81847042e02ae7535dbf5cb46abcb8af24ab8
geoffmiles <geoff.g.miles@gmail.com>
2016-01-11 19:52:54 -0500

    working user location, and semi functional autofill

---
+++ client/home/currentLocation.js
@@ -0,0 +1,13 @@
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    Session.set("lat", position.coords["latitude"]);
+    Session.set("lng", position.coords["longitude"]);
+  },
+  function(error) {
+    console.log("Error: ", error);
+  },
+  {
+    enableHighAccuracy: true
+  }
+  );
+}
--- client/home/home.js
+++ client/home/home.js
@@ -9,8 +9,10 @@ Deps.autorun(function () {
   if (Mapbox.loaded()){
 	    L.mapbox.accessToken = 'pk.eyJ1IjoiZ2VvZmZtaWxlcyIsImEiOiJjaWphZ2xrODIwMDk5dHdrbno1c3R1a3R5In0.MmoFAbmhiqY0Vqcbzmon7Q';
 		var map = L.mapbox.map('map', 'mapbox.streets', {
-		    zoomControl: false
-		}).setView([40, -74.50], 9);
+		    zoomControl: true
+		}).setView([40, -74.50], 9).addControl(L.mapbox.geocoderControl('mapbox.places', {
+        autocomplete: true
+    }));
 
 		// move the attribution control out of the way
 		map.attributionControl.setPosition('bottomleft');
@@ -33,9 +35,50 @@ Deps.autorun(function () {
 
 		var directionsInstructionsControl = L.mapbox.directions.instructionsControl('instructions', directions)
 		    .addTo(map);
-		
+
+		var geo_options = {
+		    enableHighAccuracy: true, 
+		    maximumAge        : 3000,
+		    timeout           : 9000
+		};
+
+		var watch;
+
+		document.getElementById("button").onclick = function() {
+
+		    if ( !navigator.geolocation ){
+		        errorsDiv.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+		        return;
+		    }
+		  
+		    // Geo Position
+		    navigator.geolocation.getCurrentPosition( success, error, geo_options );
+
+		};
+
+		function success(position) {      
+		    var lat  = position.coords.latitude,
+		        lng = position.coords.longitude;
+		    map.panTo( [lat,lng] );
+		    map.setZoom( 17 );
+		    directions.setOrigin( L.latLng(lat,lng) );
+		    mapbox-directions-origin-input.classList.add( 'z-lower' );
+		    geoWatch( position );
+		    positionMarker.addTo(map);
+		};
+
+		 function watchSuccess( position ) {
+		    positionMarker.setLatLng( L.latLng( position.coords.latitude, position.coords.longitude ) );
+		}
+
+		function error() {
+		    errorsDiv.innerHTML = "Unable to retrieve your location";
+		};
+
+		function geoWatch( position ) {
+		    watch = navigator.geolocation.watchPosition( watchSuccess, error, geo_options);
+		}
+
+
 	}
 });
-
-
-
